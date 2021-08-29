2445133b4b274111e8787c6f81d4000bb30bd216
yogesh upadhyay <yogeshh2021@gmail.com>
2021-08-29 14:04:16 +0530

    Header Updated

--- src/components/Heading/Header.js
+++ src/components/Heading/Header.js
@@ -19,6 +19,17 @@ const Header = () => {
                     className="typist-text"
                 />
             </h3>
+            <div className="d-flex justify-content-center">
+                <a className="m-2" href="https://github.com/YogeshUpdhyay" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-github fa-2x social-links"></i>
+                </a>
+                <a className="m-2" href="https://www.linkedin.com/in/yogesh-upadhyay-8169b4190/" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-linkedin fa-2x social-links"></i>
+                </a>
+                <a className="m-2" href="https://www.hackerrank.com/yogeshh2021" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-hackerrank fa-2x social-links"></i>
+                </a>
+            </div>
         </div>
     )
 }
