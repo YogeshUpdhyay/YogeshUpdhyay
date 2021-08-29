09f614667a58b5b704548e7e8ff6ca63ca8e7eae
yogesh upadhyay <yogeshh2021@gmail.com>
2021-08-29 23:47:20 +0530

    3d rendering started

--- src/components/Top3d/Top3d.js
+++ src/components/Top3d/Top3d.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState, Suspense } from 'react'
 import * as THREE from 'three/src/Three'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import {useGLTF, useAnimations} from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 
@@ -40,13 +41,88 @@ const fallBack = () => {
     )
 }
 
-const Top3d = () => {
+const Avatar = (props) => {
+    const group = useRef()
+    const { nodes, materials, animations } = useGLTF('../../assets/newScene.glb')
+    const { actions } = useAnimations(animations, group)
 
-    const obj = useLoader(GLTFLoader, '../../assets/mech_drone/scene.gltf')
+    console.log(nodes.Hips.children[0].children[0]);
 
     return (
-        <div className="container">
-            <primitive object={obj} />
+        <group ref={group} {...props} dispose={null}>
+            <primitive object={nodes.Hips} />
+            <skinnedMesh
+                geometry={nodes.EyeLeft.geometry}
+                material={nodes.EyeLeft.material}
+                skeleton={nodes.EyeLeft.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.EyeRight.geometry}
+                material={nodes.EyeRight.material}
+                skeleton={nodes.EyeRight.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Glasses.geometry}
+                material={materials.Wolf3D_Glasses}
+                skeleton={nodes.Wolf3D_Glasses.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Hair.geometry}
+                material={materials.Wolf3D_Hair}
+                skeleton={nodes.Wolf3D_Hair.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Hands.geometry}
+                material={nodes.Wolf3D_Hands.material}
+                skeleton={nodes.Wolf3D_Hands.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Shirt.geometry}
+                material={materials.Wolf3D_Shirt}
+                skeleton={nodes.Wolf3D_Shirt.skeleton}
+            />
+            <skinnedMesh
+                name="Wolf3D_Head"
+                geometry={nodes.Wolf3D_Head.geometry}
+                material={nodes.Wolf3D_Head.material}
+                skeleton={nodes.Wolf3D_Head.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Teeth"
+                geometry={nodes.Wolf3D_Teeth.geometry}
+                material={materials.Wolf3D_Teeth}
+                skeleton={nodes.Wolf3D_Teeth.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+            />
+        </group>
+    );
+}
+
+useGLTF.preload('../../assets/newScene.glb')
+
+const Top3d = () => {
+
+    return (
+        <div className="container mt-4">
+            <Canvas 
+                style={{
+                    height: '80vh', 
+                    width: '100%'
+                }}
+                camera={{ 
+                    fov: 20, 
+                    near: 0.1, 
+                    far: 100, 
+                    position: [0, 0, 5] 
+                }}
+            >
+                <ambientLight intensity={0.5} />
+                <pointLight position={[10, 10, 10]} />
+                <Avatar />
+            </Canvas>
         </div>
     )
 }
