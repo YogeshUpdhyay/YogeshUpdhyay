3f9721851f09ee90c8fecec44854c429d4b03303
yogesh upadhyay <yogeshh2021@gmail.com>
2021-09-08 02:32:55 +0530

    App Bar Updated

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,5 +1,6 @@
 import {React, useState} from 'react'
 import './AppBar.css'
+import logo from '../../assets/logo.png'
 
 const AppBar = () => {
 
@@ -40,18 +41,25 @@ const AppBar = () => {
 
     return (
         <div className="container pt-4" style={{maxWidth: 1200}}>
-            <nav className="navbar navbar-expand-lg navbar-transperant bg-transperant">
-                <div className="container-fluid align-items-end">
-                    <h5 id="appBarName" className="text-muted m-0"><strong>Yogesh Upadhyay</strong></h5>
-                    <div className="d-flex flex-row" id="navbarSupportedContent">
+            <nav className="row">
+                <div className="col">
+                    <div className="row m-0 p-0">
+                        <img src={logo} alt="Logo" className="rounded p-0" style={{height: 45, width: 45}}/>
+                        <h5 id="appBarName" className="text-muted m-0"><strong>Yogesh Upadhyay</strong></h5>
+                    </div>
+                </div>
+                <h5 id="appBarName" className="text-muted m-0"><strong>Yogesh Upadhyay</strong></h5>
+                <div className="col">
+                    <div className="d-flex flex-row justify-content-end">
                         {aboutActive ? <h5 className="nav-item text-white m-0 pe-4 pd-2">About</h5> : 
                             <h5 className="nav-item text-muted m-0 pe-4 pd-2" onClick={() => changeActive("About")}>About</h5>}
                         {workActive ? <h5 className="nav-item text-white m-0 pe-4 pd-2">Work</h5> : 
                             <h5 className="nav-item text-muted m-0 pe-4 pd-2" onClick={() => changeActive("Work")}>Work</h5>}
-                        {projectsActive ? <h5 className="nav-item text-white m-0 pe-4 pd-2">Projects</h5> : 
-                            <h5 className="nav-item text-muted m-0 pe-4 pd-2" onClick={() => changeActive("Project")}>Projects</h5>}
+                        {projectsActive ? <h5 className="nav-item text-white m-0 pe-2 pd-2">Projects</h5> : 
+                            <h5 className="nav-item text-muted m-0 pe-2 pd-2" onClick={() => changeActive("Project")}>Projects</h5>}
                     </div>
                 </div>
+                
             </nav>
         </div>
     )
