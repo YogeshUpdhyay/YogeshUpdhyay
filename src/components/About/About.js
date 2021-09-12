87bf7da590ed5f219397453a335be84ae7b1e72b
yogesh upadhyay <yogeshh2021@gmail.com>
2021-09-12 16:01:50 +0530

    About Section Upadted

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { fadeIn } from 'react-animations'
+import Radium, {StyleRoot} from 'radium';
+
+import "./About.css"
+
+const styles = {
+    about: {
+        animation: 'x 1s',
+        animationName: Radium.keyframes(fadeIn, 'fadeIn')
+    }
+}
 
 const About = () => {
     return (
-        <div className="container mt-5">
-            <div className="col">
-                <p className="fs-1 fw-bold text-white text-center text-decoration-underline md-4">About Me</p>
+        <StyleRoot>
+            <div className="container mt-5" style={styles.about}>
+                <div className="col">
+                    <p className="fs-2 fw-bold text-white text-center m-0">About Me</p>
+                    <hr />
+                    <div className="row">
+                        <div className="col">
+                            Here goes the images
+                        </div>
+                        <div className="col">
+                            Here goes the data
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </StyleRoot>
+        
     )
 }
 
