13619a83e5eb795be87ecf5296119d20cdaa5d50
yogesh upadhyay <yogeshh2021@gmail.com>
2021-09-18 21:00:49 +0530

    Aboout Section Updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,18 +15,16 @@ const About = () => {
     return (
         <StyleRoot>
             <div className="container mt-5" style={styles.about}>
-                <div className="col">
-                    <p className="fs-2 fw-bold text-white text-center m-0">About Me</p>
-                    <hr />
-                    <div className="row">
-                        <div className="col text-white">
-                            Here goes the images
-                        </div>
-                        <div className="col text-white">
-                            Here goes the data
-                        </div>
-                    </div>
-                </div>
+                <p className="fs-2 fw-bold text-white text-center text-decoration-underline m-0">About Me</p>
+                <h4 className="text-muted text-center m-2">HEY ✌</h4>
+                <p className="text-muted text-center ms=me-4">
+                    I’m a Full Stack and App Developer, 
+                    and I specialize in writing efficient REST APIs and database design. For the backend, 
+                    I mostly use Python or Node.js.I also have good grasp over Docker and Docker Swarm. 
+                    I am proficient in app development using Flutter. I care deeply about building software that are 
+                    usable and helpful for the most number of people possible.
+                </p>
+                
             </div>
         </StyleRoot>
         
