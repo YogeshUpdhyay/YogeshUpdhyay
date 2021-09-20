bc3695d1cdb21d68308a762cd4c8d1f8eff0615d
yogesh upadhyay <yogeshh2021@gmail.com>
2021-09-20 18:08:25 +0530

    Footer Section Added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div className="container mt-5">
                 <p className="fs-2 fw-bold text-white text-center text-decoration-underline m-0">About Me</p>
                 <h4 className="text-muted text-center m-3">HEY ✌</h4>
-                <h5 className="text-muted text-center" style={{maxWidth: 1200}}>
+                <h5 className="text-muted text-center m-0 pd-4" style={{maxWidth: 1200}}>
                     I’m a Full Stack and App Developer, 
                     and I specialize in writing efficient REST APIs and database design. For the backend, 
                     I mostly use Python or Node.js.I also have good grasp over Docker and Docker Swarm. 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import './Footer.css'
+import avatarImg from '../../assets/avatar.png'
+
+const Footer = (props) => {
+    return (
+        <div className="container py-5" style={{maxWidth: 1200}}>
+            <h3 className="text-white text-bold">Get In Touch</h3>
+            <div className="row justify-content-between">
+                <div className="col-6">
+                    <div className="row">
+                        <div className="col">
+                            <h5 className="text-white my-2">
+                                If you want to work together on a project or just have a chat.
+                                Don't hesitate to get in touch.
+                            </h5>
+                        </div>
+                        <div className="col">
+                            <img src={avatarImg} alt="Avatar Thumbnail" className="avatarImage" />
+                        </div>
+                    </div>
+                    
+                    <div className="row">
+                        <a href="mailto:yogeshh2021@gmail.com" type="button" className="btn btn-danger btn-lg my-4 mx-2">
+                            Send me an email
+                        </a>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <h5 className="text-muted text-end md-2">About</h5>
+                    <h5 className="text-muted text-end md-2">Work</h5>
+                    <h5 className="text-muted text-end md-2">Projects</h5>
+                    <div className="d-flex justify-content-end">
+                        <a className="m-2" href="https://github.com/YogeshUpdhyay" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github fa-2x social-links"></i>
+                        </a>
+                        <a className="m-2" href="https://www.linkedin.com/in/yogesh-upadhyay-8169b4190/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin fa-2x social-links"></i>
+                        </a>
+                        <a className="m-2" href="https://www.hackerrank.com/yogeshh2021" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-hackerrank fa-2x social-links"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
