2c1f49608a3311b16bf461a846f4f210d17f6e09
yogesh upadhyay <yogeshh2021@gmail.com>
2021-10-07 22:51:08 +0530

    Skills Section
    Fixes #15

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,15 +7,41 @@ const About = () => {
     return (
         <Zoom>
             <div className="container mt-5" style={{maxWidth: 1200}}>
-                <p className="fs-2 fw-bold text-white text-center text-decoration-underline m-0">About Me</p>
-                <h4 className="text-muted text-center m-3">HEY ✌</h4>
-                <h5 className="text-muted text-center m-0 pd-4">
-                    I’m a Full Stack and App Developer, 
-                    and I specialize in writing efficient REST APIs and database design. For the backend, 
-                    I mostly use Python or Node.js.I also have good grasp over Docker and Docker Swarm. 
-                    I am proficient in app development using Flutter. I care deeply about building software that are 
-                    usable and helpful for the most number of people possible.
-                </h5>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <p className="fs-1 fw-bold text-white text-decoration-underline my-4">About Me</p>
+                        <h3 className="text-muted my-3">HEY ✌</h3>
+                        <h5 className="text-muted m-0 pd-4">
+                            I’m a Full Stack and App Developer, 
+                            and I specialize in writing efficient REST APIs and database design. For the backend, 
+                            I mostly use Python or Node.js.I also have good grasp over Docker and Docker Swarm. 
+                            I am proficient in app development using Flutter. I care deeply about building software that are 
+                            usable and helpful for the most number of people possible.
+                        </h5>
+                    </div>
+
+                    {/* Skills Section */}
+                    <div className="col-lg-6">
+                        <p className="fs-1 fw-bold text-white text-decoration-underline my-4">Skills</p>
+
+                        {/* Skill Progress Bar */}
+                        <h4 className="text-white">Full Stack Web</h4>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style={{width: "85%"}} aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+
+                        <h4 className="text-white my-2">Mobile Application Dev</h4>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar" style={{width: "65%"}} aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+
+                        <h4 className="text-white my-2">DevOps</h4>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar" style={{width: "55%"}} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </Zoom>
     )
