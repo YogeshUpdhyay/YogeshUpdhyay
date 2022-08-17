cb6a2df5bf71444d88bb490b736f4a70b66a79ee
YogeshUpdhyay <yogeshh2021@gmail.com>
2022-08-17 16:45:18 +0530

    Add timeline and contact

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Blogs from './components/Blogs';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Example from './components/Navbar';
 import Projects from './components/Projects';
+import Timeline from './components/Timeline';
 
 export default function App() {
   return (
     <>
     <Example />
     <Projects />
+    <Timeline />
     <Blogs />
+    <Contact />
     <Footer />
     </>
   )
