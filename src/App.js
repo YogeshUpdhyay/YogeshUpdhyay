022782a3baeefef7f687500aebda18ea35c8ec36
YogeshUpdhyay <yogeshh2021@gmail.com>
2022-11-28 21:24:45 +0530

    Starting version 2.1.1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import logo from './static/img/logo.svg';
+
+function App() {
+  return (
+    <div>
+      <div className="hero-section">
+        <div className="navbar">
+          <div className="navbar-logo">
+            
+          </div>
+          <div className="navbar-group">
+            <div className="navbar-item">About</div>
+            <div className="navbar-item">Work</div>
+          </div>
+          <div className="navbar-menuicon">
+            <i className=''></i>
+          </div>
+        </div>
+        <div className="hero-container">
+          <div className="hero-container-cta"></div>
+          <div className="hero-container-image"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
